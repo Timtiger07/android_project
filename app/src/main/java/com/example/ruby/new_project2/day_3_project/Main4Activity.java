7ec64c1897dbd88caadb748228cb8f8691bc2296
Sapna Khatri <sapna.khatri@cgt.co.in>
2018-05-25 18:05:00 +0530

    Dialog box for Login and polishing the app

--- app/src/main/java/com/example/ruby/new_project2/day_3_project/Main4Activity.java
+++ app/src/main/java/com/example/ruby/new_project2/day_3_project/Main4Activity.java
@@ -1,9 +1,12 @@
 package com.example.ruby.new_project2.day_3_project;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.ContextThemeWrapper;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +15,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.ruby.new_project2.R;
+
+import static android.support.v4.os.LocaleListCompat.create;
 
 public class Main4Activity extends AppCompatActivity {
 
@@ -40,10 +45,33 @@ public class Main4Activity extends AppCompatActivity {
 
     }
 
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
 
     public void login(View v){
-    Toast.makeText(getBaseContext(),"ID: "+ed1.getText().toString(),Toast.LENGTH_SHORT).show();
-    Toast.makeText(getBaseContext(),"Password: "+ed1.getText().toString(),Toast.LENGTH_SHORT).show();
+        if(!isValidEmail(ed1.getText().toString())){
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+            // Setting Dialog Title
+            alertDialog.setTitle("Alert Dialog");
+
+            // Setting Dialog Message
+            alertDialog.setMessage("Wrong E-Mail");
+
+            AlertDialog dialog = alertDialog.create();
+
+            // Setting OK Button
+            alertDialog.setNegativeButton("Ok!!", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+
+            // Showing Alert Message
+
+            alertDialog.show();
+        }
     }
 
 }
